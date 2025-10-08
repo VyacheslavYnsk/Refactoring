4f5f4779e13791fb0c01f9aa618040511f765357
Alina <alinaens2005@gmail.com>
2025-10-08 22:00:31 +0700

    созданы бронирования и покупки без почты

---
+++ Refactoring/Models/Dto/Payment.cs
@@ -0,0 +1,10 @@
+public class Payment : Entity
+{
+    public Guid ClientId { get; set; }
+    public Guid PurchaseId { get; set; }
+
+    public PaymentStatusEnum Status { get; set; } = PaymentStatusEnum.PENDING;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }
+}
